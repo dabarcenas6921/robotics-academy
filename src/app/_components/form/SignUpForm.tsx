e74838b5faa199756920f93d7a95b928605255c9
David Barcenas <dabarcenas6921@gmail.com>
2023-11-05 17:40:24 -0500

    sign up page

--- src/app/_components/form/SignUpForm.tsx
+++ src/app/_components/form/SignUpForm.tsx
@@ -4,15 +4,21 @@ import Link from "next/link";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
-export default function SignInForm() {
-  const FormSchema = z.object({
-    email: z.string().min(1, "Email is required").email("Invalid email"),
-    password: z
-      .string()
-      .min(1, "Password is required")
-      .min(8, "Password must have at least 8 characters"),
-  });
-
+export default function SignUpForm() {
+  const FormSchema = z
+    .object({
+      email: z.string().min(1, "Email is required").email("Invalid email"),
+      password: z
+        .string()
+        .min(1, "Password is required")
+        .min(8, "Password must have at least 8 characters"),
+      name: z.string().min(1, "Name is required").max(30),
+      confirmPassword: z.string().min(1, "Password confirmation is required"),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      path: ["confirmPassword"],
+      message: "Passwords do not match",
+    });
   const {
     register,
     handleSubmit,
@@ -22,6 +28,8 @@ export default function SignInForm() {
     defaultValues: {
       email: "",
       password: "",
+      name: "",
+      confirmPassword: "",
     },
   });
 
@@ -34,12 +42,26 @@ export default function SignInForm() {
       <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-            Sign in to your account
+            Sign up for a new account
           </h1>
           <form
             className="space-y-4 md:space-y-6"
             onSubmit={handleSubmit(onSubmit)}
           >
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900">
+                Your Name
+              </label>
+              <input
+                type="name"
+                {...register("name")}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                placeholder="Enter your name here"
+              />
+              {errors.name && (
+                <p className="text-red-600">{errors.name.message}</p>
+              )}
+            </div>
             <div>
               <label
                 htmlFor="email"
@@ -76,20 +98,37 @@ export default function SignInForm() {
                 <p className="text-red-600">{errors.password.message}</p>
               )}
             </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                {...register("confirmPassword")}
+                placeholder="Re-enter your password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+              />
+              {errors.confirmPassword && (
+                <p className="text-red-600">{errors.confirmPassword.message}</p>
+              )}
+            </div>
 
             <button
               type="submit"
               className="w-full text-white bg-gold hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-black font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
-              Sign in
+              Sign Up
             </button>
             <p className="text-sm font-light text-gray-500">
-              Don’t have an account yet?
+              Already have an account?
               <Link
-                href="/auth/sign-up"
+                href="/auth/sign-in"
                 className="px-2 font-medium text-primary-600 hover:underline"
               >
-                Sign up
+                Sign In
               </Link>
             </p>
           </form>
