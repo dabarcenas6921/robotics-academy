20115cf97bcad46c806b6f4a7b946e53ee38e251
David Barcenas <dabarcenas6921@gmail.com>
2023-11-06 01:06:45 -0500

    fully functioning login and register

--- src/app/_components/form/SignUpForm.tsx
+++ src/app/_components/form/SignUpForm.tsx
@@ -58,10 +58,10 @@ export default function SignUpForm() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+    <div className="flex flex-col items-center justify-center px-4 sm:px-6 py-8 mx-auto md:h-screen lg:justify-center lg:py-0">
+      <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md lg:max-w-lg xl:max-w-2xl 2xl:max-w-4xl">
+        <div className="p-6 space-y-6 sm:p-8 md:p-10 lg:space-y-8">
+          <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 sm:text-3xl md:text-4xl">
             Sign up for a new account
           </h1>
           <form
@@ -69,23 +69,25 @@ export default function SignUpForm() {
             onSubmit={handleSubmit(onSubmit)}
           >
             <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900">
+              <label className="block mb-2 text-sm font-medium text-gray-900 sm:text-lg md:text-xl">
                 Your Name
               </label>
               <input
                 type="name"
                 {...register("name")}
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-base md:text-lg rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 sm:p-4 md:p-5"
                 placeholder="Enter your name here"
               />
               {errors.name && (
-                <p className="text-red-600">{errors.name.message}</p>
+                <p className="mt-2 text-red-600 text-sm sm:text-base md:text-lg">
+                  {errors.name.message}
+                </p>
               )}
             </div>
             <div>
               <label
                 htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900"
+                className="block mb-2 text-sm font-medium text-gray-900 sm:text-lg md:text-xl"
               >
                 Your email
               </label>
@@ -93,17 +95,19 @@ export default function SignUpForm() {
                 type="email"
                 {...register("email")}
                 id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-base md:text-lg rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 sm:p-4 md:p-5"
                 placeholder="mail@example.com"
               />
               {errors.email && (
-                <p className="text-red-600">{errors.email.message}</p>
+                <p className="mt-2 text-red-600 text-sm sm:text-base md:text-lg">
+                  {errors.email.message}
+                </p>
               )}
             </div>
             <div>
               <label
                 htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900"
+                className="block mb-2 text-sm font-medium text-gray-900 sm:text-lg md:text-xl"
               >
                 Password
               </label>
@@ -112,16 +116,18 @@ export default function SignUpForm() {
                 {...register("password")}
                 id="password"
                 placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-base md:text-lg rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 sm:p-4 md:p-5"
               />
               {errors.password && (
-                <p className="text-red-600">{errors.password.message}</p>
+                <p className="mt-2 text-red-600 text-sm sm:text-base md:text-lg">
+                  {errors.password.message}
+                </p>
               )}
             </div>
             <div>
               <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900"
+                htmlFor="confirmPassword"
+                className="block mb-2 text-sm font-medium text-gray-900 sm:text-lg md:text-xl"
               >
                 Confirm Password
               </label>
@@ -129,23 +135,25 @@ export default function SignUpForm() {
                 type="password"
                 {...register("confirmPassword")}
                 placeholder="Re-enter your password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-base md:text-lg rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 sm:p-4 md:p-5"
               />
               {errors.confirmPassword && (
-                <p className="text-red-600">{errors.confirmPassword.message}</p>
+                <p className="mt-2 text-red-600 text-sm sm:text-base md:text-lg">
+                  {errors.confirmPassword.message}
+                </p>
               )}
             </div>
 
             <button
               type="submit"
-              className="w-full text-white bg-gold hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-black font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              className="w-full text-white bg-gold hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-black font-medium rounded-lg text-sm sm:text-lg md:text-xl px-5 py-3 sm:px-6 sm:py-4 md:px-8 md:py-5 text-center"
             >
               Sign Up
             </button>
-            <p className="text-sm font-light text-gray-500">
+            <p className="text-sm font-light text-gray-500 sm:text-base md:text-lg">
               Already have an account?
               <Link
-                href="/auth/sign-in"
+                href="/sign-in"
                 className="px-2 font-medium text-primary-600 hover:underline"
               >
                 Sign In
