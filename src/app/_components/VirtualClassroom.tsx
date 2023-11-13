ff2a5d57592c38a45f7d86d8374dc870b8481934
Julio Arroyo <julioarroyo613@gmail.com>
2023-11-13 14:33:28 -0500

    Worked on Footer fix

--- src/app/_components/VirtualClassroom.tsx
+++ src/app/_components/VirtualClassroom.tsx
@@ -1,4 +1,4 @@
-import virtualclass from "../../../public/VirtualClass.png";
+import virtualclass from "../../../public/virtualclass.png";
 import Image from "next/image";
 
 export default function VirtualClassroom() {
--- src/app/footer/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import fiucomms from "../../../public/fiucomm.jpg";
-import nsflogo from "../../../public/NSF_logo.png";
-import compenglogo from "../../../public/FIUcomp.png";
-import Image from "next/image";
