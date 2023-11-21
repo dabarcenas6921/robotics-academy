ea3d2a5d74640a302d7a0ac921bb0f21b6ede335
David Barcenas <dabarcenas6921@gmail.com>
2023-11-21 01:38:39 -0500

    fixed cookies

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,19 +1,20 @@
 import NextAuth from "next-auth";
+import { Role } from "@prisma/client";
 
 declare module "next-auth" {
   interface User {
     email: string;
-    role: string;
+    role: Role;
   }
 
   interface Session {
     user: User & {
       email: string;
-      role: string;
+      role: Role;
     };
     token: {
       email: string;
-      role: string;
+      role: Role;
     };
   }
 }
